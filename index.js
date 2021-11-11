e0b5798da9aa873558f671d79cec7f4da0cfb356
Eduardo <eduardo.carlos.siqueira@gmail.com>
2021-11-11 12:41:59 -0300

    Limpando o index

--- index.js
+++ index.js
@@ -2,14 +2,7 @@ import consonantValue from "./consonantValue.js";
 import dataReverse from "./dataReverse.js";
 import stockList from "./stockList.js";
 import towerBuilder from "./towerBuilder.js";
+import tribonacci from "./tribonacci.js";
 
-function main() {
-  console.log(consonantValue("zodiacs"));
-  console.log(consonantValue("chruschtschov"));
-  console.log(consonantValue("khrushchev"));
-  console.log(consonantValue("strength"));
-  console.log(consonantValue("catchphrase"));
-  console.log(consonantValue("twelfthstreet"));
-  console.log(consonantValue("mischtschenkoana"));
-}
+function main() {}
 main();
